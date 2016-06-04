eca3ac506f58ee0c1d3d09e0f5f748fcee118729
Roberto <morgadoluengo@gmail.com>
2016-06-04 16:58:40 +0200

    validaciones con modalform

--- src/boletin/admin.py
+++ src/boletin/admin.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-
+from .forms import RegistradoForm
 from .models import Registrado
-class AdminRegistrado(admin.ModelAdmin):
-    #
-    list_display = ['__unicode__',"nombre", "codigo_postal", "timestamp", "actualizado"]
-    class Meta:
-        model = Registrado #ponemos el nombre del modelo al que hacemos referencia
 
+
+class AdminRegistrado(admin.ModelAdmin):
+    list_display = ['__unicode__',"nombre", "codigo_postal", "timestamp", "actualizado"]
+    form = RegistradoForm
 
 #registramos modelo en la vista de admin
 admin.site.register(Registrado, AdminRegistrado)
+
--- src/boletin/forms.py
+++ src/boletin/forms.py
@@ -1,6 +1,29 @@
 #formulatio de
 from django import forms
 
+from .models import Registrado
+
+#formulario para mostra en la vista
+
+class RegistradoForm(forms.ModelForm):
+    class Meta:
+        #modelo que suamos
+        model = Registrado
+        #campos que usaremos
+        fields = ["nombre","email"]
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        email_base, proveedor = email.split("@")
+        dominio,extension = proveedor.split(".")
+        if not extension == "co":
+            raise forms.ValidationError("Tienes que introducir correo acabado en .co")
+        return email
+
+
 class RegForm(forms.Form):
     nombre_form = forms.CharField(max_length=100)
     edad = forms.IntegerField()
+
+
+
